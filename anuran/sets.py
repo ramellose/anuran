bff1184e3e91f448a93e312a0f352445cc6184de
ramellose <lisa.rottjers@kuleuven.be>
2019-11-12 12:05:14 +0100

    Changed selection of networks for subsampling: speedup and fewer memory issues

--- anuran/sets.py
+++ anuran/sets.py
@@ -14,7 +14,7 @@ __license__ = 'Apache 2.0'
 import seaborn as sns
 import logging.handlers
 import pandas as pd
-from random import sample
+import random
 from itertools import combinations
 from scipy.special import binom
 import os
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def generate_sizes(networks, random, degree, sign,
+def generate_sizes(networks, random_models, degree_models, sign,
                    set_operation, core, fractions, prev, perm, sizes, combos=None):
     """
     This function carries out set operations on all networks provided in
@@ -44,8 +44,8 @@ def generate_sizes(networks, random, degree, sign,
     for the degree-preserving model.
 
     :param networks: List of input networks
-    :param random: Dictionary with permuted input networks without preserved degree distribution
-    :param degree: Dictionary with permuted input networks with preserved degree distribution
+    :param random_models: Dictionary with permuted input networks without preserved degree distribution
+    :param degree_models: Dictionary with permuted input networks with preserved degree distribution
     :param sign: If true, sets take sign information into account.
     :param set_operation: Type of set operation to carry outo
     :param core: Number of processor cores
@@ -65,7 +65,8 @@ def generate_sizes(networks, random, degree, sign,
             c = combos[x]
         else:
             c = None
-        combined_networks = _sample_combinations(combos=c, networks=networks, random=random, degree=degree,
+        combined_networks = _sample_combinations(combos=c, networks=networks,
+                                                 random_models=random_models, degree_models=degree_models,
                                                  group=x, fractions=fractions, prev=prev, perm=perm, sign=sign,
                                                  set_operation=set_operation, sizes=sizes)
         # run size inference in parallel
@@ -77,8 +78,8 @@ def generate_sizes(networks, random, degree, sign,
     return all_results
 
 
-def generate_sample_sizes(networks, random,
-                          degree, sign,
+def generate_sample_sizes(networks, random_models,
+                          degree_models, sign,
                           set_operation, core, fractions, prev, perm, sizes, limit, number):
     """
     This function wraps the the generate_sizes function
@@ -86,8 +87,8 @@ def generate_sample_sizes(networks, random,
     This shows the effect of increasing sample number on set size.
 
     :param networks: List of input networks
-    :param random: List of permuted input networks without preserved degree distribution
-    :param degree: List of permuted input networks with preserved degree distribution
+    :param random_models: List of permuted input networks without preserved degree distribution
+    :param degree_models: List of permuted input networks with preserved degree distribution
     :param sign: If true, sets take sign information into account.
     :param set_operation: Type of set operation to carry out
     :param core: Number of processor cores
@@ -108,22 +109,30 @@ def generate_sample_sizes(networks, random,
         all_combinations[x] = []
         for i in seq:
             n = binom(len(networks[x]), i)
+            max_num = n
             if type(limit) == int:
                 if limit < n:
                     n = limit
-            combos = combinations(range(len(networks[x])), i)
-            all_combinations[x].extend(sample(list(combos), int(n)))
-            # generate list of combinations
-        # iterables are all groups of networks
-        # this can be a huge file in memory, so be careful!
-        # maybe run in separate iterations
-    results = generate_sizes(networks=networks, random=random, degree=degree, sign=sign,
+            # for sampling random numbers
+            # if the number of combinations is small enough,
+            # we can use the iterator
+            # if not, better to sample networks randomly
+            # gives possibility of duplicates
+            # however, better than having to store huge iterator as list
+            if max_num == n:
+                combos = list(combinations(range(len(networks[x])), i))
+            else:
+                combos = list()
+                for j in range(n):
+                    combos.append(random.sample(range(len(networks[x])), i))
+            all_combinations[x].extend(combos)
+    results = generate_sizes(networks=networks, random_models=random_models, degree_models=degree_models, sign=sign,
                              set_operation=set_operation, core=core, fractions=fractions,
                              prev=prev, perm=perm, sizes=sizes, combos=all_combinations)
     return results
 
 
-def _sample_combinations(networks, random, degree, group, fractions, prev, perm, sign, set_operation, sizes, combos=None):
+def _sample_combinations(networks, random_models, degree_models, group, fractions, prev, perm, sign, set_operation, sizes, combos=None):
     """
     This function generates an iterable containing all information required
     to add new rows to a pandas dataframe.
@@ -131,8 +140,8 @@ def _sample_combinations(networks, random, degree, group, fractions, prev, perm,
 
     :param combos: List of networks to combine
     :param networks: List of networks belonging to group x
-    :param random: List of randomized null models belonging to group x
-    :param degree: List of degree-preserving null models belonging to group x
+    :param random_models: List of randomized null models belonging to group x
+    :param degree_models: List of degree-preserving null models belonging to group x
     :param group: Name of group x
     :param fractions: Size of core
     :param prev: Prevalence of core
@@ -155,10 +164,10 @@ def _sample_combinations(networks, random, degree, group, fractions, prev, perm,
                              'sign': sign,
                              'fraction': None,
                              'prev': None})
-        subrandom = {'random': [random[group]['random'][y] for y in item]}
-        subdegree = {'degree': [degree[group]['degree'][y] for y in item]}
+        subrandom = {'random': [random_models[group]['random'][y] for y in item]}
+        subdegree = {'degree': [degree_models[group]['degree'][y] for y in item]}
         for j in range(perm):
-            degreeperm = [sample(subdegree['degree'][r], 1)[0] for r in range(len(subdegree['degree']))]
+            degreeperm = [random.sample(subdegree['degree'][r], 1)[0] for r in range(len(subdegree['degree']))]
             all_networks.append({'networks': degreeperm,
                                  'name': 'Degree',
                                  'group': os.path.basename(group),
@@ -167,7 +176,7 @@ def _sample_combinations(networks, random, degree, group, fractions, prev, perm,
                                  'sign': sign,
                                  'fraction': None,
                                  'prev': None})
-            randomperm = [sample(subrandom['random'][r], 1)[0] for r in range(len(subrandom['random']))]
+            randomperm = [random.sample(subrandom['random'][r], 1)[0] for r in range(len(subrandom['random']))]
             all_networks.append({'networks': randomperm,
                                  'name': 'Random',
                                  'group': group,
@@ -186,7 +195,7 @@ def _sample_combinations(networks, random, degree, group, fractions, prev, perm,
                     subrandom['core'][frac][c] = list()
                     subdegree['core'][frac][c] = list()
                     for n in range(len(networks[group])):
-                        degreeperm = [degree[group]['core'][frac][c][n][y] for y in item]
+                        degreeperm = [degree_models[group]['core'][frac][c][n][y] for y in item]
                         all_networks.append({'networks': degreeperm,
                                              'name': 'Degree',
                                              'group': group,
@@ -195,7 +204,7 @@ def _sample_combinations(networks, random, degree, group, fractions, prev, perm,
                                              'sign': sign,
                                              'fraction': frac,
                                              'prev': c})
-                        randomperm = [random[group]['core'][frac][c][n][y] for y in item]
+                        randomperm = [random_models[group]['core'][frac][c][n][y] for y in item]
                         all_networks.append({'networks': randomperm,
                                              'name': 'Random',
                                              'group': group,
