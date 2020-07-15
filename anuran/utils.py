959baa561a423efba8482ce06222ef6427b96421
ramellose <lisa.rottjers@kuleuven.be>
2020-07-15 11:57:19 +0200

    Changed type check

--- anuran/utils.py
+++ anuran/utils.py
@@ -335,7 +335,7 @@ def _generate_rows(values):
     except KeyError:
         logger.error('Could not unpack dictionary!', exc_info=True)
     full_name = name + ' networks'
-    if fraction != None:
+    if fraction is not None:
         name += ' size: ' + str(fraction) + ' prev:' + str(prev)
     data = list()
     data.append({'Network': name,
