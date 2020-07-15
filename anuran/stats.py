e029ba1d170296c11d200c94a44fdb6a061923d1
ramellose <lisa.rottjers@kuleuven.be>
2020-07-15 10:46:56 +0200

    Changed variable reported in logger to more informative variable

--- anuran/stats.py
+++ anuran/stats.py
@@ -301,7 +301,7 @@ def compare_set_sizes(set_sizes):
                         test = normaltest(vals)
                         if test[1] < 0.05:
                             logger.warning('The values do not appear to follow a normal distribution '
-                                           'for model: ' + nulltype + ' and set size: ' + str(size))
+                                           'for model: ' + nulltype + ' and set: ' + op)
                     p = _value_outside_range(size, vals)
                     statsframe = _generate_stat_rows(statsframe, group=group, comparison=nulltype,
                                                      operation=op, p=p, ptype='Set sizes')
